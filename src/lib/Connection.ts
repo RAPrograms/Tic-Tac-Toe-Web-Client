173506f206c8e0080174119aeb084281a884a947
Dog2657 <RAPrograms@outlook.com>
2025-06-23 16:49:17 +0100

    Added request system in ws

---
+++ src/lib/Connection.ts
@@ -0,0 +1,67 @@
+export default class ServerConnection{
+    #connection: WebSocket
+    #activeRequests: Record<string, (value: [any, string | null] | PromiseLike<[any, string | null]>) => void>
+
+    constructor(connection: WebSocket){
+        this.#connection = connection
+        this.#activeRequests = {}
+
+        this.#connection.addEventListener("message", (data) => {this.handleMessage(data)});
+    }
+
+    private handleMessage({data}: any){
+        const res = JSON.parse(data)
+        if(res["Id"]){
+            const func = this.#activeRequests[res["Id"]]
+            if(func == undefined){
+                console.warn("Received a ws request without a vaild active request")
+                return
+            }
+
+            const {Status, Result} = res
+
+            if(Status == "Successful")
+                return func([Result, null])
+            
+            return func([null, Result])
+        }
+
+        console.log(res)
+    }
+
+
+    request(method: String, data: any){
+        const id = (new Date).toISOString()
+
+        this.#connection.send(JSON.stringify({
+            Id: id,
+            Method: method,
+            Data: data
+        }))
+
+        return new Promise<[any | undefined, string | null]>((resolve) => {
+            this.#activeRequests[id] = resolve
+        })
+    }
+
+    static open(uri: string){
+        return new Promise<ServerConnection | undefined>((resolve) => {
+            try {
+                const socket = new WebSocket(uri)
+
+                socket.addEventListener("error", (e) => {
+                    console.warn("Unable to connect to server")
+                    resolve(undefined)
+                })
+
+
+                socket.addEventListener("open", () => {
+                    resolve(new ServerConnection(socket))
+                })
+            } catch (error) {
+                console.error(error)
+                resolve(undefined)
+            }
+        })
+    }
+}
